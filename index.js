64f0570650e55c30ef010fa1fb3b08215d4f9b0a
brian ambundo <brian.katiechi9@gmail.com>
2022-06-03 20:22:53 +0300

    commit tree eaec3dda848f83b13ead30bd399c9f4d3e211635

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+'use strict'
+
+const employee = {
+    "name":"Sam",
+    "streetAddress":"11 Broadway"
+}
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj= {...employee}
+    newObj[key] = value
+    return newObj
+}
+
+console.log(updateEmployeeWithKeyAndValue(employee,"jina","nyumbani"))
+console.log(employee)
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign(employee,{[key]:value})
+}
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee,"nyumba","nambaTano"))
+console.log(employee)
+
+function deleteFromEmployeeByKey(employee, key){
+    let duplicatedEmployee= Object.assign({},employee)
+    delete duplicatedEmployee[key]
+return duplicatedEmployee
+     
+}
+console.log(deleteFromEmployeeByKey(employee,"streetAddress"))
+console.log(employee)
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    let duplicatedEmployee2= Object.assign(employee)
+    delete duplicatedEmployee2[key]
+    return duplicatedEmployee2
+}
+console.log(destructivelyDeleteFromEmployeeByKey(employee, "streetAddress"))
+
+
+
